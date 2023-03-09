f871cb480577f319abd7419c5fdad15e2b7324e1
xandro2021 <xandrocastro2021@gmail.com>
2023-03-08 18:23:00 -0600

    fix javascript linter issues

--- js/cards.js
+++ js/cards.js
@@ -1,54 +1,54 @@
 const Cards = [
-    {
-      img: {
-        src: 'build/img/work01.jpg',
-        alt: 'first project',
-      },
-      description: {
-        title: 'Tonic',
-        extraInfo: 'Canopy.Back End Dev.2015',
-        paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-        technologies: 'html.css.javascript',
-      },
+  {
+    img: {
+      src: 'build/img/work01.jpg',
+      alt: 'first project',
     },
-    {
-      img: {
-        src: 'build/img/work02.jpg',
-        alt: 'Second project',
-      },
-      description: {
-        title: 'Multi-Post Stories',
-        extraInfo: 'Canopy.Back End Dev.2015',
-        paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-        technologies: 'html.css.javascript',
-      },
+    description: {
+      title: 'Tonic',
+      extraInfo: 'Canopy.Back End Dev.2015',
+      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      technologies: 'html.css.javascript',
     },
-    {
-      img: {
-        src: 'build/img/work03.jpg',
-        alt: 'Third project',
-      },
-      description: {
-        title: 'Tonic',
-        extraInfo: 'Canopy.Back End Dev.2015',
-        paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-        technologies: 'html.css.javascript',
-      },
+  },
+  {
+    img: {
+      src: 'build/img/work02.jpg',
+      alt: 'Second project',
     },
-    {
-      img: {
-        src: 'build/img/work04.jpg',
-        alt: 'Fourth project',
-      },
-      description: {
-        title: 'Tonic',
-        extraInfo: 'Canopy.Back End Dev.2015',
-        paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-        technologies: 'html.css.javascript',
-      },
+    description: {
+      title: 'Multi-Post Stories',
+      extraInfo: 'Canopy.Back End Dev.2015',
+      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      technologies: 'html.css.javascript',
     },
-  ];
-  
-  export default {
-    Cards,
-  };
+  },
+  {
+    img: {
+      src: 'build/img/work03.jpg',
+      alt: 'Third project',
+    },
+    description: {
+      title: 'Tonic',
+      extraInfo: 'Canopy.Back End Dev.2015',
+      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      technologies: 'html.css.javascript',
+    },
+  },
+  {
+    img: {
+      src: 'build/img/work04.jpg',
+      alt: 'Fourth project',
+    },
+    description: {
+      title: 'Tonic',
+      extraInfo: 'Canopy.Back End Dev.2015',
+      paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      technologies: 'html.css.javascript',
+    },
+  },
+];
+
+export default {
+  Cards,
+};
